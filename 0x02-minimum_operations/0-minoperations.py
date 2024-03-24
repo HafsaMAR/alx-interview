ecc4779997e276e7e8535349e1ac1e1abe77eb44
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-24 17:21:26 +0000

    minimum operation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -10,8 +10,9 @@ def minOperations(n):
         return 0
     
     dp = [0] * (n + 1)
-    for i in range (2, n+1):
-        dp[i] = i
+    for i in range (2, n + 1):
+        dp[i] = i # worst case
+
         for j in range(2, int(i**0.5) + 1):
             if i % j == 0:
                 dp[i] = min(dp[i], dp[j] + dp[i // j])
