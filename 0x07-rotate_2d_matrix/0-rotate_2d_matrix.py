64561cfcd9fd467c3e0aa6caf482b8a303214240
HafsaMAR <marouche.hafsa@gmail.com>
2024-04-25 22:14:38 +0100

    code fix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,13 +2,13 @@
 """Rotate 2D Matrix"""
 
 
-def rotate_2d_matrix(matrix):
+def rotate_2d_matrix(matrix: 'list[list[int]]') -> None:
     """Rotate a given n x n 2D matrix by 90 degrees clockwise."""
     for i, row in enumerate(zip(*matrix[::-1])):
         matrix[i] = list(row)
 
 
 if __name__ == '__main__':
-    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    matrix: 'list[list[int]]' = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     rotate_2d_matrix(matrix)
     print(matrix)
